5ceb71a361e09573fbcc3d7b87af30061b18d6cf
kirtigrkist <kirtikumargrkist@gmail.com>
2013-10-13 00:22:20 +0530

    added 4th line in kirti

--- src/MainTest.java
+++ src/MainTest.java
@@ -7,5 +7,6 @@ public class MainTest {
 		System.out.println("Main function added");
 		System.out.println("yes program can run without main");
 		System.out.println("This is kirti branch");
+		System.out.println("Added to kirti");
 	}
 }
