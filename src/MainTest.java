f5a43e4972ec065aa41ac5fb1dd8fe68dea7838a
kirtigrkist <kirtikumargrkist@gmail.com>
2013-10-12 20:35:24 +0530

    main function added

--- src/MainTest.java
+++ src/MainTest.java
@@ -3,4 +3,8 @@ public class MainTest {
 	static{
 		System.out.println("This is a static block");
 	}
+	public static void main(String args[]){
+		System.out.println("Main function added");
+		System.out.println("But program can run without main");
+	}
 }
