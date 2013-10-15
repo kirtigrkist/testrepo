a7b4f0be053605ebfe417632cf916bcf56afb74b
kirtigrkist <kirtikumargrkist@gmail.com>
2013-10-15 22:14:44 +0530

    added test class

---
+++ src/TestClass.java
@@ -0,0 +1,10 @@
+
+public class TestClass {
+	public TestClass() {
+		System.out.println("Main function added");
+		System.out.println("yes program can run without main");
+		System.out.println("This is kirti branch");
+		System.out.println("This is master branch");
+	}
+
+}
