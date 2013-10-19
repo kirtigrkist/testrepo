a63f1c0d90fe0545ed81d756fb9bb48221593c2b
kirtigrkist <kirtikumargrkist@gmail.com>
2013-10-19 23:48:00 +0530

    pulled changes

--- src/TestClass.java
+++ src/TestClass.java
@@ -3,9 +3,9 @@ public class TestClass {
 		System.out.println("Test Class added");
 		System.out.println("yes program can run without main");
 		System.out.println("This is kirti branch");
-		System.out.println("This is master branch");
-		System.out.println("Added 5th line");
-		System.out.println("Added 6th line");
+		System.out.println("This is kirti branch");
+		System.out.println("Added fifth line");
+		System.out.println("Added sixth line");
 	}
 
 }
