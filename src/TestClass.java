bf91e1f5d8c8345725e39334b0debd7f8eb5bf6f
kirtigrkist <kirtikumargrkist@gmail.com>
2013-10-19 23:34:38 +0530

    Update TestClass.java
    some changes

--- src/TestClass.java
+++ src/TestClass.java
@@ -5,7 +5,7 @@ public class TestClass {
 		System.out.println("This is kirti branch");
 		System.out.println("This is master branch");
 		System.out.println("Added 5th line");
-		System.out.println("Added sixth line");
+		System.out.println("Added 6th line");
 	}
 
 }
